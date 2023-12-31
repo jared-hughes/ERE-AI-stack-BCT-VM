ab250ff06e29c3a078934bd3837fe5254acac811
Jared Hughes <jahughes241@gmail.com>
2023-12-31 01:44:41 -0800

    Fix decrement and zeros-ones-same
    Same problem for both. A "later" rule was getting called before an "earlier" one since it was longer.

--- sources/08_zeros_ones_same.lua
+++ sources/08_zeros_ones_same.lua
@@ -1,10 +1,11 @@
 === LIST 0 ===
 
-11100: '0'
+1111000: '100'
+ 111000:  '00'
 -- Cancel opposite
 1110: ''
 1011: ''
--- Delete identicals at end
+-- Delete identicals at end, after all cancelling is done
 10100: '100'
 111100: '1100'
 -- End case
